0e8f6afd24de9e8e5d1f4654dc5ab26265be622f
Matteo Antony Mistretta <m.mistretta@reply.it>
2025-06-26 15:35:54 +0200

    Start creating single component app

---
+++ todo-list/src/01-single-component/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>Todo List</h1>
+</template>
+
+<style scoped></style>
--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,14 +1,14 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './app/App.vue'
-import router from './router'
+import App from './01-single-component/App.vue'
+// import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
