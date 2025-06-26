e19f41eb01f9d95a1ddc83526bb232c76341fcc8
Matteo Antony Mistretta <m.mistretta@reply.it>
2025-06-26 17:46:34 +0200

    Create CRUD app

--- todo-list/src/01-single-component/App.vue
+++ todo-list/src/01-single-component/App.vue
@@ -1,9 +1,41 @@
 <script setup>
-const tasks = [
+import { ref } from 'vue'
+
+const text = ref('')
+
+const tasks = ref([
   { id: 1, title: 'Learn Vue 3', completed: true },
   { id: 2, title: 'Look for a job', completed: false },
   { id: 3, title: 'Forget everything' },
-]
+])
+
+// function handleChange(event) {
+//   text.value = event.target.value
+// }
+
+function handleSubmit() {
+  // event.preventDefault()
+  const maxId = tasks.value.length ? tasks.value[tasks.value.length - 1].id : 0
+  const task = { id: maxId + 1, title: text.value }
+  tasks.value.push(task)
+
+  text.value = ''
+}
+
+function handleToggle(index) {
+  const task = tasks.value[index]
+  task.completed = !task.completed
+  // tasks.value = [...tasks.value.slice(0, index), task, ...tasks.value.slice(index + 1)]
+  // tasks.value = tasks.value.map((task) =>
+  //   task.id === id ? { ...task, completed: !task.completed } : task,
+  // )
+}
+
+function handleRemove(index) {
+  tasks.value.splice(index, 1)
+  // tasks.value = [...tasks.value.slice(0, index), ...tasks.value.slice(index + 1)]
+  // tasks.value = tasks.value.filter((task) => task.id !== id)
+}
 </script>
 
 <template>
@@ -11,33 +43,20 @@ const tasks = [
 
   <h1>Todo List</h1>
 
-  <form>
-    <input placeholder="What next?" autofocus />
+  <form @submit.prevent="handleSubmit">
+    <!-- <input placeholder="What next?" autofocus :value="text" @change="handleChange" /> -->
+    <input placeholder="What next?" autofocus v-model="text" />
     <button>Add</button>
   </form>
 
   <ul>
-    <li>
-      <span>Look for a job</span>
+    <li v-for="(task, index) of tasks" :key="task.id">
+      <span :class="{ completed: task.completed }" @click="handleToggle(index)">{{
+        task.title
+      }}</span>
       &nbsp;
-      <button>x</button>
+      <button @click="handleRemove(index)">x</button>
     </li>
-
-    <!-- <li>
-      <span class="completed">Learn Vue 3</span>
-      &nbsp;
-      <button>x</button>
-    </li>
-    <li>
-      <span>Look for a job</span>
-      &nbsp;
-      <button>x</button>
-    </li>
-    <li>
-      <span>Forget everything</span>
-      &nbsp;
-      <button>x</button>
-    </li> -->
   </ul>
 </template>
 
